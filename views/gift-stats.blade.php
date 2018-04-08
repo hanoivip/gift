44f2627686bfea549560af2b613ff531d8786ebc
Éc Min <toannguyen@Toans-MacBook-Pro.local>
2018-04-08 13:36:47 +0700

    fix

--- views/gift-stats.blade.php
+++ views/gift-stats.blade.php
@@ -1,20 +1,8 @@
 @extends('hanoivip::admin.layouts.admin')
 
-@section('title', 'Admin sinh mã')
+@section('title', 'Admin thống kê sử dụng mã')
 
 @section('content')
 
-<form method="POST" action="{{ route('gift.batch-generate') }}">
-	{{ csrf_field() }}
-	Chọn gói/hoạt động:
-	<select id="package" name="package">
-	@foreach ($packages as $pkg)
-		<option value="{{$pkg->pack_code}}">{{$pkg->name}}</option>
-	@endforeach
-	</select>
-	Số lượng mã:
-	<input id="count" name="count" value="20" />
-	<button type="submit">Sinh mã</button>
-</form>
 
 @endsection
